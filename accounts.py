a207fb4747c4408356f78f20958f0399520bdd45
WJASK90 <jaskot.w92@gmail.com>
2025-03-16 12:00:32 +0100

    Change

--- accounts.py
+++ accounts.py
@@ -3,11 +3,11 @@ from database_one import connection
 class Account:
     def __init__(self, name: str, opening_balance: float = 0.0):
         self.name = name
-        self._balance = opening_balance
+        self.balance = opening_balance
 
         connection.execute("INSERT INTO accounts(name, balance) VALUES (?, ?)",
-                           (self.name, self._balance))
-        connection.execute("SELECT @@IDENITY AS ID")
+                           (self.name, self.balance))
+        cursor = connection.execute("SELECT @@IDENTITY AS ID")
 
         self.id = cursor.fetchval()
 
@@ -22,17 +22,17 @@ class Account:
         return round(self.balance, 2)
 
     def withdraw(self, amount: float) -> float:
-        if 0 < amount <= self._balance:
-            self._balance -= amount
+        if 0 < amount <= self.balance:
+            self.balance -= amount
             print(f"Z konta {self.name} zostało wypłacone {amount} PLN")
-        return round(self._balance, 2)
+        return round(self.balance, 2)
 
-    if __name__ == '__main__':
-        account = Account('Andrzej')
-        account.deposit(10)
-        account.deposit(0.1)
-        balance = account.withdraw(5)
-        print(balance)
+if __name__ == '__main__':
+    account = Account('Andrzej')
+    account.deposit(10)
+    account.deposit(0.1)
+    balance = account.withdraw(5)
+    print(balance)
 
 
 
--- database_one.py
+++ database_one.py
@@ -1,9 +1,32 @@
+import os
+
 import pyodbc
 
-for driver in pyodbc.drivers(): #sprawdzamy jakie mamy drivery
-    print(driver)
+# for driver in pyodbc.drivers(): #sprawdzamy jakie mamy drivery
+#     print(driver)
 
+from dotenv import load_dotenv
+load_dotenv()
 
+import pyodbc
+import os
+from dotenv import load_dotenv
+#TWORZENIE BAZY DANYCH
+load_dotenv()
+# print(os.environ.get('DATABASE_PASSWORD')) #plik .env nie moze zostac pushowany na GIT bo ma haslo, nie wolno tak robic!
+database_password = os.environ.get('DATABASE_PASSWORD') #bierze z pliku haslo
+suszi_login = 'wjaskot'
+server = 'morfeusz.wszib.edu.pl'
+
+connection_string = (
+    'DRIVER={ODBC Driver 17 for SQL Server};' #musimy pokazac do jakiego serwera sie podlaczymy
+    f'SERVER={server};' #piszemy jedno pod drugim dla lepszej skladni
+    f'DATABASE={suszi_login};' # database
+    f'UID={suszi_login};' # user ID
+    f'PWD={database_password};' # password
+    'Encrypt=no' # zazwyczaj nie piszemy, bo wtedy defaultowo mamy certyfikaty a tutaj, do nauki, nie potrzebujemy (jak w SQL Microsoft ustawiasz Encryption: Optional)
+)
+connection= pyodbc.connect(connection_string)
 
 
 

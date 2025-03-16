89fbe5f440dc7ac500029ac86a466641ec4ba38c
WJASK90 <jaskot.w92@gmail.com>
2025-03-16 11:01:54 +0100

    Change

--- two.py
+++ two.py
@@ -21,16 +21,30 @@ connection= pyodbc.connect(connection_string)
 # for d in pyodbc.drivers():
 #     print(d)
 
-connection.execute("CREATE TABLE users(id int identity, name varchar(255), age int)") #tworzymy tabele komendami z SQL! Tworz table
-connection.execute("INSERT INTO users(name, age) VALUES ('Andrzej', 28), ('Maciej',30)") #dodaj do Tabeli czyli insert
+# Start Tranzakcji
+# connection.execute("CREATE TABLE users(id int identity, name varchar(255), age int)") #tworzymy tabele komendami z SQL! Tworz table
+#Start tranzakcji 1
+# connection.execute("INSERT INTO users(name, age) VALUES ('Andrzej', 28), ('Maciej',30)") #dodaj do Tabeli czyli insert
+#Commit tranzakcji 1
+# connection.commit() #uwaga trzeba zakomentowac CREATE TABLE po pierwszym stworzeniu bo inaczej program bedzie pisal
+#ze jest blad bo juz istnieje o takim imieniu!
+
+#Start tranzakcji 2 która NIE MA COMMITA
+# connection.execute("INSERT INTO users(name, age) VALUES ('Jan', 30)")
 
 cursor = connection.cursor() #kursor iteruje bo elementach bazy danych? UWAGA Kursor iteruje do przodu, nie moze sie cofać!
 
+new_name = input('Podaj nowe imię: ')
+
+cursor.execute("UPDATE users SET name='?' WHERE name = 'Nowe'", (new_name,))
+cursor.commit()
+
+print(f'{cursor.rowcount} wierszy zmienionych')
 # cursor.execute("SELECT * FROM users") #select wszystkich kolumn z Tabeli users
 
-cursor.execute("SELECT name FROM users") # z print(cursor.fetchval()) daje jako wynik ANDRZEJ
+# cursor.execute("SELECT name FROM users") # z print(cursor.fetchval()) daje jako wynik ANDRZEJ
 
-print(cursor.fetchall())
+# print(cursor.fetchall())
 
 # for row in cursor:
 #     print(row) #pokazuje nam Andrzeja i Macieja --> ROW jest krotką! Czyli TUPLE
@@ -64,6 +78,13 @@ connection.close() #zamykamy połączenia
 # for row in result:
 #     print(row)
 
-
+# cursor = connection.cursor() #kursor iteruje bo elementach bazy danych? UWAGA Kursor iteruje do przodu, nie moze sie cofać!
+#
+# new_name = input('Podaj nowe imię: ')
+#
+# cursor.execute("UPDATE users SET name='{new_name}' WHERE name = 'Andrzej'")
+# cursor.commit()
+#
+# print(f'{cursor.rowcount} wierszy zmienionych')
 
 

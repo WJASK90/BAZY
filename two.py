26b29725eba4a657529f7e70ec658d981781077d
WJASK90 <jaskot.w92@gmail.com>
2025-03-16 10:32:41 +0100

    Change

--- two.py
+++ two.py
@@ -24,18 +24,45 @@ connection= pyodbc.connect(connection_string)
 connection.execute("CREATE TABLE users(id int identity, name varchar(255), age int)") #tworzymy tabele komendami z SQL! Tworz table
 connection.execute("INSERT INTO users(name, age) VALUES ('Andrzej', 28), ('Maciej',30)") #dodaj do Tabeli czyli insert
 
-cursor = connection.cursor() #kursor iteruje bo elementach bazy danych?
+cursor = connection.cursor() #kursor iteruje bo elementach bazy danych? UWAGA Kursor iteruje do przodu, nie moze sie cofać!
 
-cursor.execute("SELECT * FROM users") #select wszystkich kolumn z Tabeli users
+# cursor.execute("SELECT * FROM users") #select wszystkich kolumn z Tabeli users
+
+cursor.execute("SELECT name FROM users") # z print(cursor.fetchval()) daje jako wynik ANDRZEJ
+
+print(cursor.fetchall())
 
 # for row in cursor:
-#     print(row) #pokazuje nam Andrzeja i Macieja --> ROW jest krotką!
+#     print(row) #pokazuje nam Andrzeja i Macieja --> ROW jest krotką! Czyli TUPLE
 
-for user_id, name, age in cursor:
-    print(user_id, name, age, sep='\n')
+# for user_id, name, age in cursor:
+#     print(user_id, name, age, sep='\n')
+
+# result = cursor.fetchall()
+# result = cursor.fetchmany(2)
+# print(result)
+
+# print(cursor.fetchval())
+
+# for row in connection.execute("SELECT name FROM users"): # jak to jest zakomentowane # cursor.execute("SELECT name FROM users") to wtedy dziala
+#     print(row)
+
+# for now in cursor:
+#     print(row[0])
+
+# print(result)
+# print(cursor.fetchone())
+# print(cursor.fetchone())
+# print(cursor.fetchall())
 
 cursor.close() #zamykamy połączenia
 connection.close() #zamykamy połączenia
+
+# for row in result:
+#     print(row)
+#
+# for row in result:
+#     print(row)
 
 
 

709c61a1e1a2d70884c0a47338ab21a7b8b1a8ed
WJASK90 <jaskot.w92@gmail.com>
2025-04-12 12:12:47 +0200

    Change

--- alchemy_orm.py
+++ alchemy_orm.py
@@ -5,6 +5,7 @@ from sqlalchemy.orm import DeclarativeBase, mapped_column, Mapped #Mapped jest k
 
 
 str255 = Annotated[str, mapped_column(String(255))]
+intpk = Annotated[int, mapped_column(Integer, primary_key=True, auto_increment=True)]
 
 Base = DeclarativeBase()
 
@@ -16,8 +17,10 @@ class Base(DeclarativeBase):
 class User(Base): #klasa o naszych Użytkownikach, którzy mają odpowiedniki w tabeli
     __tablename__ = 'user'
     
-    id: Mapped[int] = mapped_column(primary_key=True, autoincrement=True) #definiujemy zmienna na naszej Klasie i kolumne INTEGER, Klucz Glowny i autoinkrementalna
+    id: Mapped[intpk] #było = mapped_column(primary_key=True, autoincrement=True) ale dajemy intpk #definiujemy zmienna na naszej Klasie i kolumne INTEGER, Klucz Glowny i autoinkrementalna
     name: Mapped[str] = mapped_column(String()) #odpowiednik VARCHAR(MAX)
     email: Mapped[str255] #było = mapped_column(String(255)) ale dzieki Annotated nie musze tego pisac #string o rozmiarze 255 bite'ow
     login: Mapped[str] = mapped_column(String(100), default='No Login')
     middle_name: Mapped[Optional[str]] #było tak = mapped_column(String(255), nullable=True) ale dzięki optional (import na gorze) możemy usunąć
+
+
